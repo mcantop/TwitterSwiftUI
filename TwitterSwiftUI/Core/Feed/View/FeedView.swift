3750bbb76ae8aa37df310b8a949c96bcd6f09bfe
mcantop <maciejcantop@icloud.com>
2022-10-18 21:55:24 +0200

    commit 1

---
+++ TwitterSwiftUI/Core/Feed/View/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  TwitterSwiftUI
+//
+//  Created by Maciej on 18/10/2022.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
